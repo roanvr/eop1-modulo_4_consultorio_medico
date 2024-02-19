f0991841f5305abc2ca75573de9456a2240cb6d2
RodrigoValdivia <rodrigo.valdivia@usach.cl>
2024-02-19 17:56:11 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+function Consultorio(nombre, paciente) {
+    this.nombre = nombre;
+    this.paciente = paciente;
+}
+function Paciente(nombre, edad, rut, diagnostico) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.rut = rut;
+    this.diagnostico = diagnostico;
+}
